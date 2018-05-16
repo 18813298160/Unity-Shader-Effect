12ab3d363935442a201e600b7dd0e1e177f2a176
hushiyu <18813298160@139.com>
2018-05-16 08:45:43 +0800

    sort Files

---
+++ Assets/Render Image/GrayCtrl/GrayCtrl.shader
@@ -0,0 +1,36 @@
+﻿//屏幕灰度控制
+Shader "Effect/ImageEffect" {  
+    Properties {  
+        _MainTex ("Base (RGB)", 2D) = "white" {}  
+        _LuminosityAmount ("GrayScale Amount", Range(0.0, 1.0)) = 1.0  
+    }  
+    SubShader {  
+        Pass {  
+            CGPROGRAM  
+            #pragma vertex vert_img  
+            #pragma fragment frag  
+              
+            #include "UnityCG.cginc"  
+              
+            uniform sampler2D _MainTex;  
+            fixed _LuminosityAmount;  
+              
+            fixed4 frag(v2f_img i) : COLOR  
+            {  
+                //Get the colors from the RenderTexture and the uv's  
+                //from the v2f_img struct  
+                //tex2D 2D纹理查询，可获取纹理的rgba值
+                fixed4 renderTex = tex2D(_MainTex, i.uv);  
+                  
+                //Apply the Luminosity values to our render texture  
+                float luminosity = 0.299 * renderTex.r + 0.587 * renderTex.g + 0.114 * renderTex.b;  
+                fixed4 finalColor = lerp(renderTex, luminosity, _LuminosityAmount);  
+                  
+                return finalColor;  
+            }  
+              
+            ENDCG  
+        }  
+    }  
+    FallBack "Diffuse"  
+}  
---
+++ Assets/Render Image/RenderImageEffect.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 负责在编辑中实时更新图像，同时也负责从主摄像机抓取render texture，然后把该texture传递给Shader。
+/// </summary>
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public abstract class RenderImageEffect : MonoBehaviour {
+
+    public Shader curShader;
+    /// <summary>
+    /// 需要的材质
+    /// </summary>
+    private Material curMat;
+
+    public Material mat
+    {
+        get
+        {
+            if (curMat == null)
+            {
+                curMat = new Material(curShader);
+                curMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return curMat;
+        }
+    }
+
+    public virtual void Start()
+    {
+        //平台是否支持画面特效
+        if(!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+        //是否支持此shader
+        if(curShader != null && !curShader.isSupported)
+        {
+            enabled = false;
+        }
+	}
+	
+    public virtual void Update() 
+    {
+
+	}
+
+	/// <summary>
+	/// 使用内置的OnRenderImage实现画面特效
+	/// 配合Blit函数实现屏幕后处理，OnRenderImage可抓取当前的Render Texture，
+    /// 通过Blit传递给Shader，再将处理后的图像回传给Unity渲染器
+	/// </summary>
+	/// <param name="source">Source.</param>
+	/// <param name="destination">Destination.</param>
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(curShader)
+        {
+            SetShaderProperties();
+			//source会成为mat的_MainTex
+			Graphics.Blit(source, destination, mat);
+            return;
+        }
+
+        Graphics.Blit(source, destination);
+    }
+
+    public virtual void SetShaderProperties()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        if (curMat)
+            DestroyImmediate(curMat);
+    }
+}
