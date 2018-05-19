26b44474b2ac5e0bb002dcb244821df79054d9f4
hushiyu <18813298160@139.com>
2018-05-19 23:04:37 +0800

    add res

---
+++ Assets/ShaderEffect/CardEffect/CardHolder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class CardHolder : MonoBehaviour
+{
+    private Material mat;
+    private float percent = -0.3f;
+    public float waitTime = 2;
+    void Awake()
+    {
+        StartCoroutine(wait(() =>
+        {
+            mat = GetComponent<Renderer>().material;
+        }));
+	}
+
+    IEnumerator wait(Action cb = null)
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (cb != null)
+            cb.Invoke();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!mat) return;
+        percent += 0.03f * (1 - percent);
+        percent = Mathf.Clamp(percent, 0, 1);
+        mat.SetFloat("_percent", percent);        
+	}
+}
