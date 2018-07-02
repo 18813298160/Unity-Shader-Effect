992b5e0b84ed2f40806521c7f43d882e87e271ee
达尼 <hushiyu@shangyoo.com>
2018-07-02 11:41:03 +0800

    cooling

---
+++ Assets/ShaderEffect/2DEffect/Cooling/Cooling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cooling : MonoBehaviour {
+
+    public bool isUi = true;
+    private Material mat;
+    private float angle = 0;
+	// Use this for initialization
+	void Awake () {
+        if(!isUi)
+            mat = GetComponent<MeshRenderer>().material;
+        else
+            mat = GetComponent<Image>().material;
+        angle = 0;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        angle += 1f;
+        if (angle > 359) angle = 0;
+        mat.SetFloat("_Angle", angle);
+
+    }
+}
